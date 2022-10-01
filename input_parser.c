06d53fdd7c86fc3ef5e861a8fb0a1f3b2e92593e
pskytta <77061872+peetuskytta@users.noreply.github.com>
2022-10-01 15:40:40 +0300

    Quote Handling updated
    quote cleaning basic works. needs little more tweaks

--- input_parser.c
+++ input_parser.c
@@ -24,11 +24,57 @@ int	simple_input_check(char *input)
 		return (TRUE);
 }
 
+static char	*copy_quotes(char *token, int k)
+{
+	char	new[1024];
+	int		i;
+
+	i = 0;
+	ft_memset(new, '\0', 1024);
+	while (token[k] != '\0')
+	{
+		if (token[k] == DOUBLEQUOTE)
+			k++;
+		if (token[k] == '\0')
+			break;
+		new[i] = token[k];
+		k++;
+		i++;
+	}
+	new[i] = '\0';
+	//ft_putendl(new);
+	return (ft_strcpy(new, new));
+}
+
+static void	translate_quotes(t_shell *data, int i)
+{
+	char	tmp[1024];
+
+	ft_memset(tmp, '\0', 1024);
+	while (data->token[i] != NULL)
+	{
+		if (ft_strchr(data->token[i], DOUBLEQUOTE)/* || ft_strchr(data->token[i], SINGLEQUOTE)*/)
+		{
+			ft_strcat(tmp, copy_quotes(data->token[i], 0));
+			ft_memdel((void *)&(data->token[i]));
+			data->token[i] = ft_strdup(tmp);
+			ft_memset(tmp, '\0', 1024);
+			//ft_putendl(tmp);
+			i++;
+		}
+		else
+			i++;
+		// memdel token and reassign with tmp
+	}
+}
+
 void	parse_input(t_shell *data, char *input)
 {
-
 	tokenize_simple_input(data, input, 0);
-	check_expansion(data, 0);
+	if (data->quotes == TRUE)
+		translate_quotes(data, 0);
+	else
+		check_expansion(data, 0);
 	if (check_if_builtin(data) == TRUE)
 		reset_last_cmd_env(data, 0);
 	else if (initial_exec_checks(data) == TRUE)
